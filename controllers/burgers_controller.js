add3f123dd1650404fe1f12903d60ff47059dba2
Lucas Jensen <lucasjensen56@gmail.com>
2018-05-16 20:25:00 -0500

    deployed again

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -28,7 +28,7 @@ router.post("/api/burgers", function(req, res) {
 })
 
 router.put("/api/burgers/:id", function(req, res) {
-	console.log(req.params.id)
+	console.log(req.params)
 	var condition = "id = " + req.params.id;
 
 	console.log("condition", condition);

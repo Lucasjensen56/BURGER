0c405cb93131b02af18e46f89a9ae492e009fab1
Lucas Jensen <lucasjensen56@gmail.com>
2018-05-19 12:09:11 -0500

    changed localhost

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ var mysql = require("mysql");
 
 
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: "127.0.0.1",
   user: "root",
   password: "Volund56",
   database: "burgers_db"

2f728768ea2e806605641fab6756da02ded1e9e0
Lucas Jensen <lucasjensen56@gmail.com>
2018-05-21 18:00:59 -0500

    added key

--- config/connection.js
+++ config/connection.js
@@ -13,7 +13,7 @@ if (process.env.JAWSDB_URL) {
 	connection = mysql.createConnection({
 		host: "localhost",
 	  	user: "root",
-	 	password: "Volund56",
+	 	password: process.env.SECRET_KEY,
 	 	database: "burgers_db"
 
 	})
